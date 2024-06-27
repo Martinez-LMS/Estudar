2c7d9b0430419f37cff5d8362cd297ca05640574
Martinez-LMS <leo.ooosla@gmail.com>
2024-06-26 23:28:42 -0300

    add conteudo

---
+++ Sexta/Prova A2/Diogo/Migrations/20240517120536_Inicial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Diogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Folhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<double>(type: "REAL", nullable: false),
+                    Mes = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ano = table.Column<int>(type: "INTEGER", nullable: false),
+                    SalarioLiquido = table.Column<double>(type: "REAL", nullable: false),
+                    SalarioBruto = table.Column<double>(type: "REAL", nullable: false),
+                    ImpostoIRRF = table.Column<double>(type: "REAL", nullable: false),
+                    ImpostoINSS = table.Column<double>(type: "REAL", nullable: false),
+                    ImpostoFGTS = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Folhas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    CPF = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Folhas");
+
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
